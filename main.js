1bcb2737da2637cf3310ea08ba939ffbab68d08d
Jörgen Kosche <jkosche@mnementh.de>
2023-08-30 17:07:15 +0200

    load datasets

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let app =
 	status:
 	{
 	},
+	datasetlist: [],
 	datasets:
 	{
 	}
@@ -20,24 +21,45 @@ function init_datalist()
 	console.log("status: ", this.status);
 	console.log("content: ", this.responseText);
 	console.log("app: ", app);
+	if (this.status !== 200) return;
 	let datasets = JSON.parse(this.responseText);
+	app.datasetlist = datasets;
 	for (const dataset of datasets)
 	{
-		console.log(dataset);
+		let datasetinfo =
+		{
+			dir: dataset,
+		}
+		load_url("data/" + dataset + "/info.json", datasetinfo, init_datasetinfo);
 	}
+}
+
+function init_datasetinfo()
+{
+	console.log("init_datalist: ", this);
+	console.log("status: ", this.status);
+	console.log("content: ", this.responseText);
+	if (this.status !== 200)
+	{
+		return;
+	}
+	console.log("appinfo: ", this.appinfo);
+	let dataset = JSON.parse(this.responseText);
+	app.datasets[this.appinfo.dir] = dataset;
+	console.log("app: ", app);
 }
 
 function start()
 {
 	console.log("start!");
 	console.log("Papa: ", Papa);
-		const config =
-		{
-			complete: parsed_csv,
-			download: true,
-			skipEmptyLines: true,
-		}
-		Papa.parse("data/test.csv", config);
+	const config =
+	{
+		complete: parsed_csv,
+		download: true,
+		skipEmptyLines: true,
+	}
+	Papa.parse("data/test.csv", config);
 }
 
 function load_url(url, info, listener)
