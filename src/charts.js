87119e371f9c0b6da823eb2264cc3381f7c41f3e
Jörgen Kosche <jkosche@mnementh.de>
2023-10-04 14:47:59 +0200

    some refactoring

--- src/charts.js
+++ src/charts.js
@@ -3,14 +3,17 @@ function refresh_barchart_view()
 	console.log("refresh_barchart_view");
 	console.log("app.data.processed: ", app.data.processed);
 	let barchart_view_data = document.getElementById("barchart_view_data");
+	let dataview = '';
 	if (!app.data.processed)
 	{
-		barchart_view_data.innerHTML = "Für die gewählte Selektion sind keine Daten verfügbar!";
-		return;
+		dataview = "Für die gewählte Selektion sind keine Daten verfügbar!";
 	}
-	let dataview = `
-	<div>
-		Dieser Inhalt ist noch nicht implementiert.
-	</div>`;
+	else
+	{
+		dataview = `
+		<div>
+			Dieser Inhalt ist noch nicht implementiert.
+		</div>`;
+	}
 	barchart_view_data.innerHTML = dataview;
 }
