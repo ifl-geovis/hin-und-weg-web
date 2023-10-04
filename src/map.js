3fad6131f367ac465c616dd8e0866f2ae104b4d2
Jörgen Kosche <jkosche@mnementh.de>
2023-10-04 15:53:13 +0200

    preparations for dynamic mapping

--- src/map.js
+++ src/map.js
@@ -11,22 +11,32 @@ function init_map()
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', mapconfig).addTo(app.map.map);
 }
 
-function show_geojson_layer()
+function map_style(feature)
 {
 	let style =
 	{
 		fillColor: 'white',
 		weight: 1.5,
 		opacity: 1,
-		color: 'grey',
+		color: get_feature_border_color(feature.properties[app.selection.dataset.id_property]),
 		fillOpacity: 0.5,
 	};
+	return style;
+}
+
+function show_geojson_layer()
+{
 	if (app.map.datalayer) app.map.datalayer.removeFrom(app.map.map);
-	if (app.data.geodata) app.map.datalayer = L.geoJSON(app.data.geodata, {style: style});
-	console.log("show_geojson_layer:", app.map);
+	if (app.data.geodata) app.map.datalayer = L.geoJSON(app.data.geodata, {style: map_style});
 	if (app.map.datalayer)
 	{
 		app.map.datalayer.addTo(app.map.map);
 		app.map.map.fitBounds(app.map.datalayer.getBounds());
 	}
+}
+
+function get_feature_border_color(feature_id)
+{
+	console.log("get_feature_border_color:", feature_id);
+	return 'grey';
 }
