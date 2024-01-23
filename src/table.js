db8eb614d66422263c8c951133054b2fd0c1dfd6
Jörgen Kosche <jkosche@mnementh.de>
2024-01-23 10:10:27 +0100

    add ui for sorting the table

--- src/table.js
+++ src/table.js
@@ -10,13 +10,26 @@ function refresh_table_view()
 	}
 	let numberrow = "Anzahl";
 	if (app.selection.data_interpretation === "migration_rate") numberrow = "Wanderungsrate";
+	const arrow = (app.selection.tablesort_ascending) ? "⬆" : "⬇";
+	const arrowdiv = `<div id="table_heading_arrow">${arrow}</div>`;
+	let fromarr = '';
+	let toarr = '';
+	let numberarr = '';
+	let spaces = '';
+	if (app.selection.tablesort === "from") fromarr = arrowdiv;
+	if (app.selection.tablesort === "to") toarr = arrowdiv;
+	if (app.selection.tablesort === "number")
+	{
+		numberarr = arrowdiv;
+		spaces = "&nbsp;&nbsp;&nbsp;";
+	}
 	let dataview = `
 	<table>
 	<tr class="header">
-		<th></th>
-		<th>Von</th>
-		<th>Nach</th>
-		<th>${numberrow}</th>
+		<th onclick="tablesort(null)"></th>
+		<th onclick="tablesort('from')">${fromarr}Von</th>
+		<th onclick="tablesort('to')">${toarr}Nach</th>
+		<th onclick="tablesort('number')">${numberarr}${numberrow}${spaces}</th>
 	</tr>`;
 	let odd = true;
 	for (let row of app.data.processed)
@@ -32,6 +45,14 @@ function refresh_table_view()
 	}
 	dataview += "</table>";
 	table_view_data.innerHTML = dataview;
+}
+
+function tablesort(column)
+{
+	if (column === app.selection.tablesort) app.selection.tablesort_ascending = !app.selection.tablesort_ascending;
+	else app.selection.tablesort_ascending = false;
+	app.selection.tablesort = column;
+	refresh_table_view();
 }
 
 function refresh_statistics_view()
